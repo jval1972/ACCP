c19fdbeab3f2ee0451471e04f800f15ea11f389c
jval1972 <jvalavanis@gmail.com>
2022-02-04 06:25:14 +0200

    Error messages table

--- acc_error.pas
+++ acc_error.pas
@@ -1,20 +1,31 @@
+//------------------------------------------------------------------------------
+//
+//  ACCP Compiler - ACS Compiler (Pascal)
+//  Based on ACC code by by Ben Gokey.
+//
+//  Copyright (C) 1995 by Raven Software
+//  Copyright (C) 2022 by Jim Valavanis
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+//  02111-1307, USA.
+//
+//------------------------------------------------------------------------------
+//  Site  : https://sourceforge.net/projects/delphidoom/
+//------------------------------------------------------------------------------
 
-/(**************************************************************************
-/(**
-/(** error.c
-/(**
-/(**************************************************************************
-
-// HEADER FILES ------------------------------------------------------------
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <string.h>
-#include 'common.h'
-#include 'error.h'
-#include 'token.h'
-#include 'misc.h'
+{$I Doom32.inc}
 
 const
   ERR_NONE = 0;
@@ -88,177 +99,96 @@ const
   ERR_UNKNOWN_PRTYPE = 77;
   ERR_BAD_CHARACTER = 78;
 
-// MACROS ------------------------------------------------------------------
+const
+  ERROR_FILE_NAME = 'acs.err';
 
-#define ERROR_FILE_NAME 'acs.err'
+var
+  ErrorFileName: string;
 
-// TYPES -------------------------------------------------------------------
-
-// EXTERNAL FUNCTION PROTOTYPES --------------------------------------------
-
-// PUBLIC FUNCTION PROTOTYPES ----------------------------------------------
-
-// PRIVATE FUNCTION PROTOTYPES ---------------------------------------------
-
-static char *ErrorFileName;
-static char *ErrorText(error_t error);
-
-// EXTERNAL DATA DECLARATIONS ----------------------------------------------
-
-extern char acs_SourceFileName[MAX_FILE_NAME_LENGTH];
-
-// PUBLIC DATA DEFINITIONS -------------------------------------------------
-
-// PRIVATE DATA DEFINITIONS ------------------------------------------------
-
-static struct
-begin
-  error_t number;
-  char *name;
-  end; ErrorNames[] :=
-  begin
-  ERR_MISSING_SEMICOLON,
-  'Missing semicolon.',
-  ERR_MISSING_LPAREN,
-  'Missing '('.',
-  ERR_MISSING_RPAREN,
-  'Missing ')'.',
-  ERR_MISSING_SCRIPT_NUMBER,
-  'Missing script number.',
-  ERR_IDENTIFIER_TOO_LONG,
-  'Identifier too long.',
-  ERR_STRING_TOO_LONG,
-  'String too long.',
-  ERR_FILE_NAME_TOO_LONG,
-  'File name too long.',
-  ERR_BAD_CHARACTER,
-  'Bad character in script text.',
-  ERR_ALLOC_PCODE_BUFFER,
-  'Failed to allocate PCODE buffer.',
-  ERR_PCODE_BUFFER_OVERFLOW,
-  'PCODE buffer overflow.',
-  ERR_TOO_MANY_SCRIPTS,
-  'Too many scripts.',
-  ERR_SAVE_OBJECT_FAILED,
-  'Couldn't save object file.',
-  ERR_MISSING_LPAREN_SCR,
-  'Missing '(' in script definition.',
-  ERR_INVALID_IDENTIFIER,
-  'Invalid identifier.',
-  ERR_REDEFINED_IDENTIFIER,
-  'Redefined identifier.',
-  ERR_MISSING_COMMA,
-  'Missing comma.',
-  ERR_BAD_VAR_TYPE,
-  'Invalid variable type.',
-  ERR_TOO_MANY_SCRIPT_ARGS,
-  'Too many script arguments.',
-  ERR_MISSING_LBRACE_SCR,
-  'Missing opening ' begin ' in script definition.',
-  ERR_MISSING_RBRACE_SCR,
-  'Missing closing ' end;' in script definition.',
-  ERR_TOO_MANY_MAP_VARS,
-  'Too many map variables.',
-  ERR_TOO_MANY_SCRIPT_VARS,
-  'Too many script variables.',
-  ERR_MISSING_WVAR_INDEX,
-  'Missing index in world variable declaration.',
-  ERR_BAD_WVAR_INDEX,
-  'World variable index out of range.',
-  ERR_MISSING_WVAR_COLON,
-  'Missing colon in world variable declaration.',
-  ERR_MISSING_SPEC_VAL,
-  'Missing value in special declaration.',
-  ERR_MISSING_SPEC_COLON,
-  'Missing colon in special declaration.',
-  ERR_MISSING_SPEC_ARGC,
-  'Missing argument count in special declaration.',
-  ERR_CANT_READ_FILE,
-  'Couldn't read file.',
-  ERR_CANT_OPEN_FILE,
-  'Couldn't open file.',
-  ERR_CANT_OPEN_DBGFILE,
-  'Couldn't open debug file.',
-  ERR_INVALID_DIRECTIVE,
-  'Invalid directive.',
-  ERR_BAD_DEFINE,
-  'Non-numeric constant found in #define.',
-  ERR_INCL_NESTING_TOO_DEEP,
-  'Include nesting too deep.',
-  ERR_STRING_LIT_NOT_FOUND,
-  'String literal not found.',
-  ERR_INVALID_DECLARATOR,
-  'Invalid declarator.',
-  ERR_BAD_LSPEC_ARG_COUNT,
-  'Incorrect number of special arguments.',
-  ERR_BAD_ARG_COUNT,
-  'Incorrect number of arguments.',
-  ERR_UNKNOWN_IDENTIFIER,
-  'Identifier has not been declared.',
-  ERR_MISSING_COLON,
-  'Missing colon.',
-  ERR_BAD_EXPR,
-  'Syntax error in expression.',
-  ERR_BAD_CONST_EXPR,
-  'Syntax error in constant expression.',
-  ERR_NO_DIRECT_VER,
-  'Internal function has no direct version.',
-  ERR_ILLEGAL_EXPR_IDENT,
-  'Illegal identifier in expression.',
-  ERR_EXPR_FUNC_NO_RET_VAL,
-  'Function call in expression has no return value.',
-  ERR_MISSING_ASSIGN_OP,
-  'Missing assignment operator.',
-  ERR_INCDEC_OP_ON_NON_VAR,
-  ''++' or '--' used on a non-variable.',
-  ERR_MISSING_RBRACE,
-  'Missing ' end;' at end of compound statement.',
-  ERR_INVALID_STATEMENT,
-  'Invalid statement.',
-  ERR_BAD_DO_STATEMENT,
-  'Do statement not followed by 'while' or 'until'.',
-  ERR_BAD_SCRIPT_DECL,
-  'Bad script declaration.',
-  ERR_CASE_OVERFLOW,
-  'Internal Error: stack overflow.',
-  ERR_BREAK_OVERFLOW,
-  'Internal Error: Break stack overflow.',
-  ERR_CONTINUE_OVERFLOW,
-  'Internal Error: Continue stack overflow.',
-  ERR_STATEMENT_OVERFLOW,
-  'Internal Error: Statement overflow.',
-  ERR_MISPLACED_BREAK,
-  'Misplaced BREAK statement.',
-  ERR_MISPLACED_CONTINUE,
-  'Misplaced CONTINUE statement.',
-  ERR_CASE_NOT_IN_SWITCH,
-  'CASE must appear in case statement.',
-  ERR_DEFAULT_NOT_IN_SWITCH,
-  'DEFAULT must appear in case statement.',
-  ERR_MULTIPLE_DEFAULT,
-  'Only 1 DEFAULT per case allowed.',
-  ERR_EXPR_STACK_OVERFLOW,
-  'Expression stack overflow.',
-  ERR_EXPR_STACK_EMPTY,
-  'Tried to POP empty expression stack.',
-  ERR_UNKNOWN_CONST_EXPR_PCD,
-  'Unknown PCD in constant expression.',
-  ERR_BAD_RADIX_CONSTANT,
-  'Radix out of range in integer constant.',
-  ERR_BAD_ASSIGNMENT,
-  'Syntax error in multiple assignment statement.',
-  ERR_OUT_OF_MEMORY,
-  'Out of memory.',
-  ERR_TOO_MANY_STRINGS,
-  'Too many strings.',
-  ERR_UNKNOWN_PRTYPE,
-  'Unknown cast type in print statement.',
-  ERR_NONE, NULL
+type
+  errormessage_t = record
+    number: Integer;
+    name: string[40];
   end;
 
-// CODE --------------------------------------------------------------------
+const
+  NUMERRORNAME = 71;
 
-// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
+var
+  ErrorNames: array[0..NUMERRORNAME - 1] of errormessage_t = (
+    (number: ERR_MISSING_SEMICOLON; name: 'Missing semicolon.'),
+    (number: ERR_MISSING_LPAREN; name: 'Missing ''(''.'),
+    (number: ERR_MISSING_RPAREN; name: 'Missing '')''.'),
+    (number: ERR_MISSING_SCRIPT_NUMBER; name: 'Missing script number.'),
+    (number: ERR_IDENTIFIER_TOO_LONG; name: 'Identifier too long.'),
+    (number: ERR_STRING_TOO_LONG; name: 'String too long.'),
+    (number: ERR_FILE_NAME_TOO_LONG; name: 'File name too long.'),
+    (number: ERR_BAD_CHARACTER; name: 'Bad character in script text.'),
+    (number: ERR_ALLOC_PCODE_BUFFER; name: 'Failed to allocate PCODE buffer.'),
+    (number: ERR_PCODE_BUFFER_OVERFLOW; name: 'PCODE buffer overflow.'),
+    (number: ERR_TOO_MANY_SCRIPTS; name: 'Too many scripts.'),
+    (number: ERR_SAVE_OBJECT_FAILED; name: 'Couldn''t save object file.'),
+    (number: ERR_MISSING_LPAREN_SCR; name: 'Missing ''('' in script definition.'),
+    (number: ERR_INVALID_IDENTIFIER; name: 'Invalid identifier.'),
+    (number: ERR_REDEFINED_IDENTIFIER; name: 'Redefined identifier.'),
+    (number: ERR_MISSING_COMMA; name: 'Missing comma.'),
+    (number: ERR_BAD_VAR_TYPE; name: 'Invalid variable type.'),
+    (number: ERR_TOO_MANY_SCRIPT_ARGS; name: 'Too many script arguments.'),
+    (number: ERR_MISSING_LBRACE_SCR; name: 'Missing opening ''{'' in script definition.'),
+    (number: ERR_MISSING_RBRACE_SCR; name: 'Missing closing ''}'' in script definition.'),
+    (number: ERR_TOO_MANY_MAP_VARS; name: 'Too many map variables.'),
+    (number: ERR_TOO_MANY_SCRIPT_VARS; name: 'Too many script variables.'),
+    (number: ERR_MISSING_WVAR_INDEX; name: 'Missing index in world variable declaration.'),
+    (number: ERR_BAD_WVAR_INDEX; name: 'World variable index out of range.'),
+    (number: ERR_MISSING_WVAR_COLON; name: 'Missing colon in world variable declaration.'),
+    (number: ERR_MISSING_SPEC_VAL; name: 'Missing value in special declaration.'),
+    (number: ERR_MISSING_SPEC_COLON; name: 'Missing colon in special declaration.'),
+    (number: ERR_MISSING_SPEC_ARGC; name: 'Missing argument count in special declaration.'),
+    (number: ERR_CANT_READ_FILE; name: 'Couldn''t read file.'),
+    (number: ERR_CANT_OPEN_FILE; name: 'Couldn''t open file.'),
+    (number: ERR_CANT_OPEN_DBGFILE; name: 'Couldn''t open debug file.'),
+    (number: ERR_INVALID_DIRECTIVE; name: 'Invalid directive.'),
+    (number: ERR_BAD_DEFINE; name: 'Non-numeric constant found in #define.'),
+    (number: ERR_INCL_NESTING_TOO_DEEP; name: 'Include nesting too deep.'),
+    (number: ERR_STRING_LIT_NOT_FOUND; name: 'String literal not found.'),
+    (number: ERR_INVALID_DECLARATOR; name: 'Invalid declarator.'),
+    (number: ERR_BAD_LSPEC_ARG_COUNT; name: 'Incorrect number of special arguments.'),
+    (number: ERR_BAD_ARG_COUNT; name: 'Incorrect number of arguments.'),
+    (number: ERR_UNKNOWN_IDENTIFIER; name: 'Identifier has not been declared.'),
+    (number: ERR_MISSING_COLON; name: 'Missing colon.'),
+    (number: ERR_BAD_EXPR; name: 'Syntax error in expression.'),
+    (number: ERR_BAD_CONST_EXPR; name: 'Syntax error in constant expression.'),
+    (number: ERR_NO_DIRECT_VER; name: 'Internal function has no direct version.'),
+    (number: ERR_ILLEGAL_EXPR_IDENT; name: 'Illegal identifier in expression.'),
+    (number: ERR_EXPR_FUNC_NO_RET_VAL; name: 'Function call in expression has no return value.'),
+    (number: ERR_MISSING_ASSIGN_OP; name: 'Missing assignment operator.'),
+    (number: ERR_INCDEC_OP_ON_NON_VAR; name: '''++'' or ''--'' used on a non-variable.'),
+    (number: ERR_MISSING_RBRACE; name: 'Missing ''}'' at end of compound statement.'),
+    (number: ERR_INVALID_STATEMENT; name: 'Invalid statement.'),
+    (number: ERR_BAD_DO_STATEMENT; name: 'Do statement not followed by ''while'' or ''until''.'),
+    (number: ERR_BAD_SCRIPT_DECL; name: 'Bad script declaration.'),
+    (number: ERR_CASE_OVERFLOW; name: 'Internal Error: stack overflow.'),
+    (number: ERR_BREAK_OVERFLOW; name: 'Internal Error: Break stack overflow.'),
+    (number: ERR_CONTINUE_OVERFLOW; name: 'Internal Error: Continue stack overflow.'),
+    (number: ERR_STATEMENT_OVERFLOW; name: 'Internal Error: Statement overflow.'),
+    (number: ERR_MISPLACED_BREAK; name: 'Misplaced BREAK statement.'),
+    (number: ERR_MISPLACED_CONTINUE; name: 'Misplaced CONTINUE statement.'),
+    (number: ERR_CASE_NOT_IN_SWITCH; name: 'CASE must appear in case statement.'),
+    (number: ERR_DEFAULT_NOT_IN_SWITCH; name: 'DEFAULT must appear in case statement.'),
+    (number: ERR_MULTIPLE_DEFAULT; name: 'Only 1 DEFAULT per case allowed.'),
+    (number: ERR_EXPR_STACK_OVERFLOW; name: 'Expression stack overflow.'),
+    (number: ERR_EXPR_STACK_EMPTY; name: 'Tried to POP empty expression stack.'),
+    (number: ERR_UNKNOWN_CONST_EXPR_PCD; name: 'Unknown PCD in constant expression.'),
+    (number: ERR_BAD_RADIX_CONSTANT; name: 'Radix out of range in integer constant.'),
+    (number: ERR_BAD_ASSIGNMENT; name: 'Syntax error in multiple assignment statement.'),
+    (number: ERR_OUT_OF_MEMORY; name: 'Out of memory.'),
+    (number: ERR_TOO_MANY_STRINGS; name: 'Too many strings.'),
+    (number: ERR_UNKNOWN_PRTYPE; name: 'Unknown cast type in print statement.'),
+    (number: ERR_NONE; name: '')
+  );
+  end;
+
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 //
 // ERR_Exit
 //
