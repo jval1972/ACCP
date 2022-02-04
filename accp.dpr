bb33e1a5e607e1dbaaad6b6305b7700c883c2d78
jval1972 <jvalavanis@gmail.com>
2022-02-04 05:20:48 +0200

    Added source files to main project file

--- accp.dpr
+++ accp.dpr
@@ -32,7 +32,16 @@ program accp;
 {$APPTYPE CONSOLE}
 
 uses
-  SysUtils;
+  SysUtils,
+  acc in 'acc.pas',
+  acc_common in 'acc_common.pas',
+  acc_error in 'acc_error.pas',
+  acc_misc in 'acc_misc.pas',
+  acc_parse in 'acc_parse.pas',
+  acc_pcode in 'acc_pcode.pas',
+  acc_strlist in 'acc_strlist.pas',
+  acc_symbol in 'acc_symbol.pas',
+  acc_token in 'acc_token.pas';
 
 begin
   { TODO -oUser -cConsole Main : Insert code here }
